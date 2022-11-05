f9170220e6a77594449da43b319b165927f29aa0
pengfuguo <vaemc520@qq.com>
2022-11-05 16:02:19 +0800

    1

--- esptool_server_py/esptool_server.py
+++ esptool_server_py/esptool_server.py
@@ -2,10 +2,11 @@ from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS, cross_origin
 import serial.tools.list_ports
 import subprocess
-
+import os
 ports = serial.tools.list_ports.comports()
 app = Flask(__name__, template_folder='./')
 cors = CORS(app)
+
 
 @app.route('/port_list')
 @cross_origin()
@@ -20,11 +21,16 @@ def port_list():
             pass
     return jsonify(serial_ports)
 
+
 @app.route('/execute_cmd')
 @cross_origin()
 def execute_cmd():
-    print(subprocess.Popen("./websocketd --port=8081 cat /dev/ttyS3", shell=True, stdout=subprocess.PIPE).stdout.read())
+    #os.system("./websocketd --port=8081 cat /dev/ttyS3")
+    # os.popen("./websocketd --port=8081 cat /dev/ttyS3").read()
+    return_code = subprocess.call("./websocketd --port=8081 cat /dev/ttyS3", shell=True)
+
     return "aa"
+
 
 @app.route('/')
 def index():
--- esptool_server_vue/src/views/SerialMonitor.vue
+++ esptool_server_vue/src/views/SerialMonitor.vue
@@ -18,8 +18,27 @@ export default {
   data() {
     return {
       portList: [],
-      baudRateList: ["300","600","750","1200","2400","4800","9600","19200","38400","57600","115200","230400","460800","500000","921600","1000000","2000000"],
+      baudRateList: [
+        "300",
+        "600",
+        "750",
+        "1200",
+        "2400",
+        "4800",
+        "9600",
+        "19200",
+        "38400",
+        "57600",
+        "115200",
+        "230400",
+        "460800",
+        "500000",
+        "921600",
+        "1000000",
+        "2000000",
+      ],
       info: "",
+      ws: null,
     };
   },
   mounted() {
@@ -34,22 +53,17 @@ export default {
       });
     },
     ok() {
-      const wsuri = "ws://192.168.5.190:8081/";
-      this.websock = new WebSocket(wsuri);
-      this.websock.onmessage = this.websocketonmessage;
+      this.axios.get("execute_cmd").then((res) => {
+        const wsuri = "ws://192.168.5.190:8081/";
+        this.ws = new WebSocket(wsuri);
 
-      this.info = "";
+        this.ws.onmessage = this.wsMessage;
+        this.info = "";
+      });
     },
-    websocketonmessage(e) {
+    wsMessage(e) {
       console.log(e.data);
       this.info += e.data + "<br />";
-
-      // const options = {
-      //   duration: 300,
-      //   offset: 0,
-      //   easing: Object.keys(easings),
-      // };
-      // this.$vuetify.goTo(100, options);
     },
   },
 };
